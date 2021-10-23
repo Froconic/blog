8d316d0e9a19c4859e700ac540aebdd1095c1f6c
Froconic <froconicdesign@gmail.com>
2021-10-23 09:56:35 -0600

    blog page works socials component made

---
+++ components/global/Social.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <section class="has-divider">
+      <div class="container pt-3">
+        <div class="row justify-content-center">
+          <div class="col-xl-7 col-lg-8 col-md-10">
+            <hr>
+            <div class="d-flex align-items-center">
+              <span class="text-small mr-1">Share this article:</span>
+              <div class="d-flex mx-2">
+                <NuxtLink :to="'/'" class="btn btn-round btn-primary mx-1">
+                  <img class="icon icon-sm" :src="social.facebook" alt="social.facebookAlt">
+                </NuxtLink>
+                <NuxtLink :to="'/'" class="btn btn-round btn-primary mx-1">
+                  <img class="icon icon-sm" :src="social.instagram" alt="social.instagramAlt">
+                </NuxtLink>
+                <NuxtLink :to="'/'" class="btn btn-round btn-primary mx-1">
+                  <img class="icon icon-sm" :src="social.youtube" alt="social.youtubeAlt">
+                </NuxtLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      social: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+
+</script>
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,16 +1,90 @@
 <template>
-<article>
-  <nuxt-content :document="article" />
-  
-</article>
+  <article>
+    <section class="bg-black text-light overlay min-vh-100 d-flex flex-column justify-content-end jarallax" data-jarallax data-speed="0.5">
+      <img :src="article.img" :alt="article.alt" class="opacity-60">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-10 col-xl-8">
+            <div class="d-flex justify-content-between align-items-center mb-3">
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li v-for="link of article.toc" :key="link.id" class="breadcrumb-item">
+                    <NuxtLink :to="'#${link.id}'">{{link.text}}</NuxtLink>
+                  </li>
+                </ol>
+              </nav>
+              <span class="badge badge-primary">
+                <img class="icon bg-primary" :src="article.icon" alt="heart interface icon">21</span>
+            </div>
+            <h1>{{article.title}}</h1>
+            <div class="d-flex align-items-center">
+              <NuxtLink to='/'>
+                <img :src="article.avatar" :alt="article.avatarAlt" class="avatar mr-2">
+              </NuxtLink>
+              <div>
+                <div>by <NuxtLink to='/'>{{article.author}}</NuxtLink>
+                </div>
+                <div class="text-small text-muted">{{formatDate(article.updatedAt)}}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="py-0 px-5" data-reading-position>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-7 col-lg-8 col-md-10">
+            <p class="lead">{{ article.description }}</p>
+            <p>
+              <nuxt-content :document="article" />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </article>
 </template>
 
 <script>
   export default {
-    async asyncData({ $content, params}) {
+    async asyncData({
+      $content,
+      params
+    }) {
       const article = await $content('articles', params.slug).fetch()
 
-      return { article }
-    }
+      return {
+        article
+      }
+    },
+
+    methods: {
+      formatDate(date) {
+        const options = {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        }
+        return new Date(date).toLocaleDateString('en', options)
+      }
+    },
   }
+
 </script>
+
+<style>
+  .icon.icon-link {
+    background-image: url('assets/img/icons/theme/code/thunder-circle.svg');
+    display: inline-block;
+    width: 2rem;
+    height: 2rem;
+    background-size: 2rem 2rem;
+    background-color: none;
+    color: red;
+    fill: red;
+    border-radius: 25px;
+    margin: 0 10px;
+  }
+
+</style>
